753067f0b04f56b9c52b69ead18c6884536bdb99
flhel <58229462+flhel@users.noreply.github.com>
2022-04-20 15:36:48 +0200

    Unity Objects for the Minigame
    Tools etc.
    
    + Updated Object Layers

---
+++ Assets/Scripts/Tool.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public bool isAccurate;
+}
