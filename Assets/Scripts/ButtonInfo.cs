cd6e06a551f730f1bad2f668d248ac780b47bf4c
holkerX <96183139+holkerX@users.noreply.github.com>
2022-06-23 18:18:21 +0200

    alles an shit

---
+++ Assets/Scripts/ButtonInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonInfo : MonoBehaviour
+{
+
+    public int ItemID;
+    public TMP_Text PriceTxt;
+    public TMP_Text QuantityTxt;
+    public GameObject ShopManager;
+
+    void Update()
+    {
+        PriceTxt.text = "Price: $" + ShopManager.GetComponent<ShopManagerScript>().shopItems[2, ItemID].ToString();
+        QuantityTxt.text = ShopManager.GetComponent<ShopManagerScript>().shopItems[3, ItemID].ToString();
+    }
+}
