4db8245b7325711257500969823cb008627a62b2
holkerX <96183139+holkerX@users.noreply.github.com>
2022-09-10 18:49:24 +0200

    Übergabe von manpower und expMultiplikator

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -10,6 +10,9 @@ public class GameData
     public int expMultiplikator;
     public bool abraumMatrixObjectsInitialized;
     public AbraumMatrixObject[] abraumMatrixObjects;
+    public bool abraumMatrixInitialized;
+    public int[][][] abraumMatrix;
+    public double money;
 
     public GameData()
     {
@@ -18,5 +21,8 @@ public class GameData
         this.expMultiplikator = 1;
         this.abraumMatrixObjectsInitialized = false;
         this.abraumMatrixObjects = new AbraumMatrixObject[15000];
+        abraumMatrixInitialized = false;
+        this.abraumMatrix = null;
+        this.money = 0;
     }
 }
